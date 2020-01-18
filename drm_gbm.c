b451b8887c9f7c2c2aa030c65f951643f3153022
Peter de Rivaz <peter.derivaz@gmail.com>
2020-01-18 14:22:45 +0000

    Switch to use planes to resize in hardware scaler for large monitors

--- drm_gbm.c
+++ drm_gbm.c
@@ -16,8 +16,14 @@
 
 // global variables declarations
 
+static int render_width = 1920;
+static int render_height = 1080;
+//static int render_width = 800;
+//static int render_height = 480;
+
 static int device;
 static drmModeRes *resources;
+static drmModePlaneResPtr planeResources;
 static drmModeConnector *connector;
 static uint32_t connector_id;
 static drmModeEncoder *encoder;
@@ -27,6 +33,7 @@ static struct gbm_device *gbm_device;
 static EGLDisplay display;
 static EGLContext context;
 static struct gbm_surface *gbm_surface;
+//static struct gbm_surface *gbm_surface2;
 static EGLSurface egl_surface;
        EGLConfig config;
        EGLint num_config;
@@ -61,6 +68,13 @@ uint32_t pitch;
 uint32_t fb;
 uint64_t modifier;
 
+uint32_t plane_id;
+
+// If the screen is the same resolution as the render size we can use the primary overlay (a bit faster)
+// Otherwise, we use a full screen overlay plane
+static int drm_use_primary(void) {
+	return render_width == mode_info.hdisplay && render_height == mode_info.vdisplay;
+}
 
 static drmModeConnector *find_connector (drmModeRes *resources) {
 	for (i=0; i<resources->count_connectors; i++) {
@@ -85,9 +99,20 @@ static void swap_buffers () {
 	bo = gbm_surface_lock_front_buffer (gbm_surface);
 	handle = gbm_bo_get_handle (bo).u32;
 	pitch = gbm_bo_get_stride (bo);
-	drmModeAddFB (device, mode_info.hdisplay, mode_info.vdisplay, 24, 32, pitch, handle, &fb);
-	//drmModeAddFB (device, 640, 480, 24, 32, pitch, handle, &fb);
-	drmModeSetCrtc (device, crtc->crtc_id, fb, 0, 0, &connector_id, 1, &mode_info);
+	drmModeAddFB (device, render_width, render_height, 24, 32, pitch, handle, &fb);
+	if (drm_use_primary()) {
+		drmModeSetCrtc (device, crtc->crtc_id, fb, 0, 0, &connector_id, 1, &mode_info);
+	} else {
+		drmModeSetPlane(device, plane_id, crtc->crtc_id,
+			   fb, 
+			   0, // flags do we need overlay here?
+			   0,0, //int32_t crtc_x, int32_t crtc_y,
+			   mode_info.hdisplay,mode_info.vdisplay, //uint32_t crtc_w, uint32_t crtc_h,
+			   0,0, // Q16 uint32_t src_x, uint32_t src_y,
+			   render_width << 16, render_height << 16 // uint32_t src_w, uint32_t src_h
+			   );
+	}
+	
 	if (previous_bo) {
 	  drmModeRmFB (device, previous_fb);
 	  gbm_surface_release_buffer (gbm_surface, previous_bo);
@@ -97,20 +122,42 @@ static void swap_buffers () {
 }
 
 static void draw (float progress) {
-
-glClearColor (1.0f-progress, progress, 0.0, 1.0);
-glClear (GL_COLOR_BUFFER_BIT);
-swap_buffers ();
+	glClearColor (1.0f-progress, progress, 0.0, 1.0);
+	glClear (GL_COLOR_BUFFER_BIT);
+	swap_buffers ();
 }
 
 static int match_config_to_visual(EGLDisplay egl_display, EGLint visual_id, EGLConfig *configs, int count) {
+	EGLint id;
+	for (i = 0; i < count; ++i) {
+	  if (!eglGetConfigAttrib(egl_display, configs[i], EGL_NATIVE_VISUAL_ID,&id)) continue;
+	  if (id == visual_id) return i;
+	  }
+	return -1;
+}
 
-EGLint id;
-for (i = 0; i < count; ++i) {
-  if (!eglGetConfigAttrib(egl_display, configs[i], EGL_NATIVE_VISUAL_ID,&id)) continue;
-  if (id == visual_id) return i;
-  }
-return -1;
+uint32_t find_plane(drmModePlaneResPtr resources, uint32_t crtc_id) {
+	int i;
+	for (i = 0; i < resources->count_planes; i++) {
+		uint32_t pl = resources->planes[i];
+		drmModePlanePtr ovr = drmModeGetPlane(device,pl);
+		if (!ovr)
+			continue;
+		//printf("Plane %d/%d->%d id=%d crtx_id=%d\n",i,resources->count_planes,pl,ovr->plane_id,ovr->crtc_id);
+		
+		return pl;
+
+		//if (!format_support(ovr, p->fourcc))
+		//	continue;
+
+		/*if ((ovr->possible_crtcs & (1 << pipe)) &&
+		    (ovr->crtc_id == 0 || ovr->crtc_id == p->crtc_id)) {
+			plane_id = ovr->plane_id;
+			break;
+		}*/
+	}
+	printf("No planes found\n");
+	return -1;
 }
 
 void drm_gbm_start () {
@@ -126,13 +173,15 @@ void drm_gbm_start () {
 	connector_id = connector->connector_id;
 	mode_info = connector->modes[0];
 	encoder = find_encoder (resources, connector);
+	planeResources = drmModeGetPlaneResources(device);
+	plane_id = find_plane(planeResources, encoder->crtc_id);
 	crtc = drmModeGetCrtc (device, encoder->crtc_id);
 	drmModeFreeEncoder (encoder);
 	drmModeFreeConnector (connector);
 	drmModeFreeResources (resources);
 	gbm_device = gbm_create_device (device);
-	gbm_surface = gbm_surface_create (gbm_device, mode_info.hdisplay, mode_info.vdisplay, GBM_FORMAT_XRGB8888, GBM_BO_USE_SCANOUT|GBM_BO_USE_RENDERING);
-	//gbm_surface = gbm_surface_create (gbm_device, 640, 480, GBM_FORMAT_XRGB8888, GBM_BO_USE_SCANOUT|GBM_BO_USE_RENDERING);
+	//gbm_surface = gbm_surface_create (gbm_device, mode_info.hdisplay, mode_info.vdisplay, GBM_FORMAT_XRGB8888, GBM_BO_USE_SCANOUT|GBM_BO_USE_RENDERING);
+	gbm_surface = gbm_surface_create (gbm_device, render_width, render_height, GBM_FORMAT_XRGB8888, GBM_BO_USE_SCANOUT|GBM_BO_USE_RENDERING);
 	display = eglGetDisplay (gbm_device);
 	eglInitialize (display, NULL ,NULL);
 	eglBindAPI (EGL_OPENGL_API);
@@ -144,14 +193,22 @@ void drm_gbm_start () {
 	egl_surface = eglCreateWindowSurface (display, configs[config_index], gbm_surface, NULL);
 	free(configs);
 	eglMakeCurrent (display, egl_surface, egl_surface, context);
+	
+	if (drm_use_primary()) {
+		printf("Using primary plane\n");
+	} else {
+		printf("Using overlay plane as render size %dx%d does not match display resolution %dx%d\n",render_width,render_height,mode_info.hdisplay,mode_info.vdisplay);
+	}
 }
 
 int drm_width(void) {
-	return mode_info.hdisplay;
+	//return mode_info.hdisplay;
+	return render_width;
 }
 
 int drm_height(void) {
-	return mode_info.vdisplay;
+	return render_height;
+	//return mode_info.vdisplay;
 }
 
 void drm_gbm_test(void) {
@@ -166,13 +223,16 @@ void drm_gbm_swap(void) {
 void drm_gbm_finish(void) {
 	//printf("Error status=%d\n",glGetError());
 	
-	drmModeSetCrtc (device, crtc->crtc_id, crtc->buffer_id, crtc->x, crtc->y, &connector_id, 1, &crtc->mode);
+	if (drm_use_primary()) {
+		drmModeSetCrtc (device, crtc->crtc_id, crtc->buffer_id, crtc->x, crtc->y, &connector_id, 1, &crtc->mode);
+	}
 	drmModeFreeCrtc (crtc);
 	if (previous_bo) {
 	  drmModeRmFB (device, previous_fb);
 	  gbm_surface_release_buffer (gbm_surface, previous_bo);
 	  }
 	eglDestroySurface (display, egl_surface);
+	//gbm_surface_destroy (gbm_surface2);
 	gbm_surface_destroy (gbm_surface);
 	eglDestroyContext (display, context);
 	eglTerminate (display);
